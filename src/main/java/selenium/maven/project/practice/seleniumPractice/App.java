f98057cd8031217d56eec69935689f1130eecf70
Baikunth <Baikunth@BNS>
2023-10-22 03:30:04 +0530

    Commit First

---
+++ src/main/java/selenium/maven/project/practice/seleniumPractice/App.java
@@ -0,0 +1,13 @@
+package selenium.maven.project.practice.seleniumPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
